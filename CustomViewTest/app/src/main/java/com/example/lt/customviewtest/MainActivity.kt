879163758b821fc3b80c4aa0725835c5ff765466
tong <maxdylan@163.com>
2020-04-11 08:20:52 +0800

    增加仪表盘和饼图的空activity

--- CustomViewTest/app/src/main/java/com/example/lt/customviewtest/MainActivity.kt
+++ CustomViewTest/app/src/main/java/com/example/lt/customviewtest/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.lt.customviewtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnDashboard.setOnClickListener {
+            // todo 跳转到仪表盘
+        }
+        btnPie.setOnClickListener {
+            // todo 跳转到饼图
+        }
     }
 }
